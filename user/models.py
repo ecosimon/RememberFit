ceff9de688643df4a9516f2ba1b8c09b8a15f871
normsliu <normanliu91@gmail.com>
2019-01-22 12:43:56 -0500

    created UserProfile model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,11 @@
+from django.conf import settings
 from django.db import models
-# Create your models here.
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    height = models.DecimalField(decimal_places=2, 'Round height in inches to nearest two deciml places')
+    age = models.PositiveIntegerField()
+    weight = models.DecimalField(decimal_places=2,'Round weight in inches to nearest two deciml places')
+
+    def __str__(self):
+        return self.user
