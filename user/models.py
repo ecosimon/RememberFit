daefc71049d1f8041abf2d739e3fb386cdf0cb56
ecosimon <liu_simon_eco@yahoo.com>
2019-03-15 17:01:15 -0400

    Updated UserProfile attributes, also added comments

--- user/models.py
+++ user/models.py
@@ -1,8 +1,12 @@
 from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    """
+        This model is a custom UserProfile so that each User in the future, can modify their information.
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     height = models.DecimalField(decimal_places=2,max_digits=5)
     age = models.PositiveIntegerField()
     weight = models.DecimalField(decimal_places=2,max_digits=5)
