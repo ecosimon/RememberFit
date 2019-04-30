55ff9905143616487f1def6bbcb98efa57ab4c50
ecosimon <liu_simon_eco@yahoo.com>
2019-04-30 17:32:02 -0400

    Added new field: username to identify UserProfile objects

--- user/models.py
+++ user/models.py
@@ -7,9 +7,10 @@ class UserProfile(models.Model):
         This model is a custom UserProfile so that each User in the future, can modify their information.
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    height = models.DecimalField(decimal_places=2,max_digits=5)
-    age = models.PositiveIntegerField()
-    weight = models.DecimalField(decimal_places=2,max_digits=5)
+    username = models.CharField(max_length=255, blank=True)
+    height = models.DecimalField(decimal_places=2,max_digits=5, null=True)
+    age = models.PositiveIntegerField(blank=True, null=True)
+    weight = models.DecimalField(decimal_places=2,max_digits=5, null=True)
 
     def __str__(self):
-        return self.user
+        return self.username
