69161c2103d7f05d5c0da36efc4e58ac4c5762cd
ecosimon <liu_simon_eco@yahoo.com>
2019-05-01 11:35:19 -0400

    Custom registration Form added.

--- user/forms.py
+++ user/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
-
+# Need to redo this, a custom login is needed. research authentication
 class LoginForm(AuthenticationForm):
     """
     This form extends AuthenticationForm which is a Django feature for authenticating credentials.
@@ -17,6 +17,39 @@ class LoginForm(AuthenticationForm):
     password = forms.CharField(label="Password", max_length=30,
                                widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'password'}))
 							  
+							  
+class SignUpForm(forms.Form):
+    """
+	    Default sign up form for our custom registration. Testing is required.
+    """
+    username = forms.CharField(max_length=30, blank=True)
+    emmail = forms.CharField(max_length=255, blank=True)
+	
+    def clean_username(self):
+        """
+            Clean method is called when .is_valid() is invoked. This checks if the current username is taken in auth_user.
+        """
+        cd_username = self.cleaned_data.get('username')
+
+        if not cd_username:
+            raise forms.ValidationError('You need a username in order to sign up!')
+        if (User.objects.get(username=cd_username).count() > 0):
+            raise forms.ValidationError('This user is already taken!')
+        return cd_username
+		
+    def clean_email(self):
+        """
+            Clean method is called when .is_valid() is invoked. Checks if the current email is in use.
+        """
+        cd_email = self.cleaned_data.get('email')
+		
+        if not cd_email:
+            raise forms.ValidationError('Email is required for sign up!')
+        if (User.objects.get(email=cd_email).count() > 0):
+            raise forms.ValidationError('This email is already taken!')
+        return cd_email
+		
+
 # class SignUpForm(UserCreationForm):
     # """
     # A Sign up form extending Django's built-in UserCreationForm.
