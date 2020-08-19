a19d80ddbeb4b553176c9a0e5cc3bfd9bd662e2a
simon liu <sliu.10410@gmail.com>
2020-08-19 15:34:42 -0400

    Updated information in forms and views.py

--- user/forms.py
+++ user/forms.py
@@ -9,7 +9,9 @@ class UserForm(forms.ModelForm):
         model = User
         fields = ('username','password','email')
 
-class UserProfileForm(forms.ModelForm):
-    class Meta():
-        model = UserProfile
-        fields = ('user','height','age','weight')
+# Not really sure if this is needed. This can be initialized manually from UserForm.
+# note: while user signs up, they can add their info and after registering data updates graph.
+# class UserProfileForm(forms.ModelForm):
+#     class Meta():
+#         model = UserProfile
+#         fields = ('user',)
--- user/views.py
+++ user/views.py
@@ -7,7 +7,7 @@ from django.urls import reverse
 from django.views.generic import TemplateView
 from chartjs.views.lines import BaseLineChartView
 from .models import UserProfile
-from .forms import UserProfileForm
+from .forms import UserForm
 # from .forms import SignUpForm
 
 # login_required decorator requires view to be authenticated
@@ -18,3 +18,20 @@ def home(request):
     May switch over to Class-based-views in the future when more data stats are added.
     """
     return render(request, "home.html")
+
+def register(request):
+    registered = False
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        if user_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            user_profile = UserProfile.objects.create(user=user)
+            user_profile.save()
+            registered = True
+        else:
+            print(user_form.errors)
+    else:
+        user_form = UserForm()
+    return render(request, 'signup.html', {})
